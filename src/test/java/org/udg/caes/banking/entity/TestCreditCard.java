bc7d968ecdeb3dd30e5423d629473f3616f96c2e
xerbok <xavicarbo3@gmail.com>
2026-09-21 19:23:36 +0200

    more tests

--- src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
+++ src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
@@ -13,6 +13,27 @@ public class TestCreditCard {
         cc.credit(100);                         // Act
         assertEquals(100 ,cc.getCredit());     // Assert
     }
+
+    @Test
+    void testReset(){
+        CreditCard cc = new CreditCard("test");
+        cc.credit(100);
+        cc.reset();
+        assertEquals(0, cc.getCredit());
+    }
+
+    @Test
+    void testMaxCredit(){
+        CreditCard cc = new CreditCard("test");
+        cc.maxCredit = 150;
+        assertEquals(150, cc.getMaxCredit());
+    }
+
+    @Test
+    void testGetId(){
+        CreditCard cc = new CreditCard("test");
+        assertEquals("test", cc.getId());
+    }
 }
 
 /* git config --global --unset user.name
