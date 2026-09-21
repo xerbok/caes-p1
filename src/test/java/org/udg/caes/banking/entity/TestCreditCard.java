d25adf0e138878dfc5fd223adf6e5b2baac2f720
xerbok <xavicarbo3@gmail.com>
2026-09-21 19:07:33 +0200

    primer test unit

--- src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
+++ src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
@@ -2,10 +2,19 @@ package org.udg.caes.banking.entity;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TestCreditCard {
 
     @Test
     void testCredit(){
-
+        CreditCard cc = new CreditCard("test");     // Arrange
+        cc.credit(100);                         // Act
+        assertEquals(100 ,cc.getCredit());     // Assert
     }
 }
+
+/* git config --global --unset user.name
+git config --global --unset user.email
+git config --global --list */
